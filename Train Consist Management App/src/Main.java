a9d84cd297368cf17d7214e5f5ec9f34424c18a1
ds9751-creator <ds9751@srmist.edu.in>
2026-04-01 14:40:58 +0530

    UC3

--- Train Consist Management App/src/Main.java
+++ Train Consist Management App/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("==============================================");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println("==============================================\n");
+
+        Set<String> bogies = new HashSet<>();
+
+        bogies.add("B6101");
+        bogies.add("B6102");
+        bogies.add("B6103");
+        bogies.add("B6104");
+
+        bogies.add("B6101");
+        bogies.add("B6102");
+
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
+
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+
+        System.out.println("\nUC3 uniqueness validation completed...");
     }
 }
