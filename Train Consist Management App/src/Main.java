b4167b379382a53004796b89abda9dde8097c561
ds9751-creator <ds9751@srmist.edu.in>
2026-04-01 15:20:30 +0530

    UC4

--- Train Consist Management App/src/Main.java
+++ Train Consist Management App/src/Main.java
@@ -27,27 +27,5 @@ public class Main{
         System.out.println(trainConsist);
 
         System.out.println("\nUC4 ordered consist operations completed...");
-import java.util.HashMap;
-import java.util.Map;
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("==============================================");
-        System.out.println(" UC6 - Map Bogie to Capacity (HashMap) ");
-        System.out.println("==============================================\n");
-
-        Map<String, Integer> capacityMap = new HashMap<>();
-
-        capacityMap.put("First Class", 24);
-        capacityMap.put("Cargo", 120);
-        capacityMap.put("Sleeper", 72);
-        capacityMap.put("AC Chair", 56);
-
-        System.out.println("Bogie Capacity Details:");
-
-        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
-
-        System.out.println("\nUC6 bogie-capacity mapping completed...");
     }
 }
