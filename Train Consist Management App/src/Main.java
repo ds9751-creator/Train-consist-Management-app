8a61ee21cbb3020a51b8924b38f0d94795f63640
ds9751-creator <ds9751@srmist.edu.in>
2026-04-01 15:26:07 +0530

    UC5

--- Train Consist Management App/src/Main.java
+++ Train Consist Management App/src/Main.java
@@ -23,27 +23,5 @@ public class Main{
         System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
 
         System.out.println("\nUC5 formation setup completed...");
-        System.out.println(" UC3 - Track Unique Bogie IDs ");
-        System.out.println("==============================================\n");
-
-        Set<String> bogies = new HashSet<>();
-
-        bogies.add("B6101");
-        bogies.add("B6102");
-        bogies.add("B6103");
-        bogies.add("B6104");
-
-        bogies.add("B6101");
-        bogies.add("B6102");
-
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogies);
-
-        System.out.println("\nNote:");
-        System.out.println("Duplicates are automatically ignored by HashSet.");
-
-        System.out.println("\nUC3 uniqueness validation completed...");
-
-
     }
 }
